138d83f01ddbf611fe52d7d128a2d27c237333d9
13020180015 <62000128+13020180015@users.noreply.github.com>
2020-03-10 16:34:20 +0800

    Add files via upload

---
+++ fikom.java
@@ -0,0 +1,6 @@
+public class fikom{
+  public static void main(String[] args)
+{
+  System.out.println("selesai belajar pemrograman java");
+      }
+}
---
+++ mayang.java
@@ -0,0 +1,6 @@
+public class pbo{
+  public static void main(String[] args)
+{
+  System.out.println("belajar bahasa pemrograman java");
+      }
+}
